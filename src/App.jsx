4a6b1f61b1080ceb0cd24814e857c26520402fba
drafffffff <i0.0ia@qq.com>
2023-04-09 23:11:16 +0800

    ✨ 功能(App.jsx)：添加鼠标移动事件，绑定点光源的位置到鼠标位置 🐛 修复(App.jsx)：修复页面宽度问题，添加滚动条 🎨 样式(index.css)：调整滚动区域样式，使其占据整个页面宽度

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import "./index.css";
-import { onCleanup, onMount } from "solid-js";
+import { createEffect, createSignal, onCleanup, onMount } from "solid-js";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import ScrambleText from "scramble-text";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { SSRPass } from "three/addons/postprocessing/SSRPass.js";
+import { GlitchPass } from "three/addons/postprocessing/GlitchPass.js";
 import studio from "@theatre/studio";
 import { getProject, types } from "@theatre/core";
 import projectState from "./coc.theatre-project-state.json";
 import ScrambleTextPlugin from "gsap/ScrambleTextPlugin";
+
+import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
+import { BloomPass } from "three/addons/postprocessing/BloomPass.js";
+import { AnaglyphEffect } from "three/addons/effects/AnaglyphEffect.js";
+import { RenderPixelatedPass } from "three/addons/postprocessing/RenderPixelatedPass.js";
+import { HorizontalBlurShader } from "three/addons/shaders/HorizontalBlurShader.js";
+import { VerticalBlurShader } from "three/addons/shaders/VerticalBlurShader.js";
+
 gsap.registerPlugin(ScrollTrigger, ScrambleTextPlugin);
 
 //a map function like p5
@@ -17,8 +28,15 @@ const map = (value, start1, stop1, start2, stop2) => {
 };
 
 export default function Home() {
+  const [mousePos, setMousePos] = createSignal({ x: 0, y: 0 });
+  function onMouseMove(e) {
+    setMousePos({ x: e.clientX, y: e.clientY });
+  }
+  const width = window.innerWidth || 2;
+  const height = window.innerHeight || 2;
   let title = null;
-  let camera, scene, renderer;
+  let camera, scene, renderer, composer, ssrPass, renderPixelPass, renderScene;
+  let whitePointLight, greenPointLight;
   let threeContainer = null;
   const textureLoader = new THREE.TextureLoader();
   const project = getProject("coc", { state: projectState });
@@ -37,6 +55,59 @@ export default function Home() {
       1000
     );
     camera.position.y = 9;
+    //add whitePointLight
+    whitePointLight = new THREE.PointLight(0xffffff, 20);
+    // whitePointLight.distance = 2000;
+    whitePointLight.position.set(1, 0, 1);
+    scene.add(whitePointLight);
+    //add greenPointLight
+    greenPointLight = new THREE.PointLight(0x00ff00, 50);
+    greenPointLight.position.set(1, 0, -1);
+    // greenPointLight.distance = 2000;
+    scene.add(greenPointLight);
+    const cylinderPhysical = new THREE.MeshPhysicalMaterial({
+      color: 0xffffff, // 基础颜色为黑色
+      metalness: 1, // 金属度为1，表示完全是金属
+      roughness: 0.1, // 粗糙度为0，表示非常光滑
+    });
+    camera.lookAt(0, -50, 0);
+    const sphereMap = textureLoader.load("/texture/spheremap.png");
+    const spherePhysical = new THREE.MeshBasicMaterial({
+      color: 0x000000,
+      // map: sphereMap,
+    });
+    // spherePhysical.emissiveIntensity = 0.5;
+
+    const loader = new GLTFLoader().setPath("/model/");
+    loader.load("scene.gltf", function (gltf) {
+      const sphere = gltf.scene.children.find((child) => {
+        return child.name === "球体002";
+      });
+      const cylinder = gltf.scene.children.find((child) => {
+        return child.name === "Cylinder";
+      });
+      console.log(sphere);
+      cylinder.material = cylinderPhysical;
+      sphere.material = spherePhysical;
+      scene.add(gltf.scene);
+    });
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    threeContainer.appendChild(renderer.domElement);
+    // composer
+    composer = new EffectComposer(renderer);
+    ssrPass = new SSRPass({
+      renderer,
+      scene,
+      camera,
+      width: innerWidth,
+      height: innerHeight,
+    });
+
+    // const glitchPass = new GlitchPass();
+    // composer.addPass(glitchPass);
+    composer.addPass(ssrPass);
     //theatre anime
     const cameraAnime = sheet.object("camera", {
       position: types.compound({
@@ -44,43 +115,51 @@ export default function Home() {
         y: types.number(camera.position.y, { min: -10, max: 10 }),
         z: types.number(camera.position.z, { min: -10, max: 10 }),
       }),
+      caveRoughness: types.number(0.1, { range: [0, 1] }),
+      pixelate: types.number(6, { range: [1, 30] }),
     });
     cameraAnime.onValuesChange((v) => {
       camera.position.x = v.position.x;
       camera.position.y = v.position.y;
       camera.position.z = v.position.z;
+      cylinderPhysical.roughness = v.caveRoughness;
     });
-    camera.lookAt(0, 0, 0);
-    const texture = textureLoader.load("/matcap/1.png");
-    const loader = new GLTFLoader().setPath("/model/");
-    loader.load("scene.gltf", function (gltf) {
-      const cylinder = gltf.scene.children[0].children.find((child) => {
-        return child.name === "Cylinder";
-      });
-      const cylinder002 = gltf.scene.children[0].children.find((child) => {
-        return child.name === "Cylinder002";
-      });
-      const cylinder001 = gltf.scene.children[0].children.find((child) => {
-        return child.name === "Cylinder001";
-      });
-      console.log(cylinder);
-      const cylinderMaterial = new THREE.MeshMatcapMaterial({
-        matcap: texture,
-        transparent: true,
-        opacity: 0.5,
-      });
-      cylinder.material = cylinderMaterial;
-      cylinder002.material = cylinderMaterial;
-      cylinder001.material = cylinderMaterial;
-      scene.add(gltf.scene);
-    });
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    threeContainer.appendChild(renderer.domElement);
   }
+
   function animate() {
     requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+    // composer.render(scene, camera);
+    whitePointLight.position.y = camera.position.y - 10;
+    greenPointLight.position.y = camera.position.y - 10;
+    whitePointLight.position.x = map(
+      mousePos().x,
+      0,
+      window.innerWidth,
+      -10,
+      10
+    );
+    whitePointLight.position.z = map(
+      mousePos().y,
+      0,
+      window.innerHeight,
+      -10,
+      10
+    );
+    greenPointLight.position.x = map(
+      mousePos().x,
+      0,
+      window.innerWidth,
+      -10,
+      10
+    );
+    greenPointLight.position.z = map(
+      mousePos().y,
+      0,
+      window.innerHeight,
+      -10,
+      10
+    );
+    composer.render(scene, camera);
   }
   function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -88,28 +167,26 @@ export default function Home() {
     renderer.setSize(window.innerWidth, window.innerHeight);
   }
   let timmer = null;
+  //binding poniteLigte x z pos to mousepos
   onMount(() => {
     if (import.meta.env.DEV) {
       studio.initialize();
     }
     threeInit();
     new ScrambleText(title, scrambConfig).start();
-    timmer = setInterval(() => {
-      new ScrambleText(title, scrambConfig).start();
-    }, 3000);
+    // timmer = setInterval(() => {
+    //   new ScrambleText(title, scrambConfig).start();
+    // }, 3000);
     window.addEventListener("resize", onWindowResize, false);
     animate();
     //total scroll
-    const rootHeight = document.documentElement.scrollHeight;
     ScrollTrigger.create({
       trigger: "#root",
       start: "top top",
-      // endTrigger: ".footer",
       end: `bottom bottom`,
       scrub: 1,
       // markers: true,
       onUpdate: (self) => {
-        console.log(self.progress);
         sheet.sequence.position = map(self.progress, 0, 1, 0, 4);
       },
     });
@@ -141,7 +218,7 @@ export default function Home() {
     });
   });
   return (
-    <div class="main">
+    <div class="main" onMouseMove={onMouseMove}>
       <div className="threescene " ref={threeContainer}></div>
       <div class="scroll">
         <div class="section title px-8 py-4">
